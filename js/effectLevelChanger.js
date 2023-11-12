b85956b13b43e1b4f208508bed5898f544477a02
https://github.com/ghost160111 <jalol100.ghost@yandex.com>
2023-11-12 17:53:27 +0500

    Отделил функцию которая которая добавляет и настраивает фильтр для изображения как отдельный модуль

---
+++ js/effectLevelChanger.js
@@ -0,0 +1,79 @@
+import domVariables from './domVariables.js';
+import effectMapList from './EffectMapList.js';
+
+const {
+  imgUploadPreviewContainer,
+  effectsRadioBtns,
+  effectLevelValueInput ,
+  effectLevelValueSlider,
+  imgUploadEffectLevelContainer,
+} = domVariables;
+
+// default slider config:
+let sliderConfig = {
+  start: [0, 100],
+  connect: true,
+  step: 1,
+  range: {
+    min: 0,
+    max: 100
+  }
+};
+
+// effectNameList array that will contain list of effect names:
+const effectNameList = [];
+for (const [key] of effectMapList.entries()) {
+  effectNameList.push(key);
+}
+
+// creating first prototype of noUiSlider
+noUiSlider.create(effectLevelValueSlider, sliderConfig);
+
+let effectValue = 100;
+
+const effectLevelChanger = () => {
+  for (let i = 0; i < effectsRadioBtns.length; ++i) {
+    const radioClickHandler = () => {
+      if (effectsRadioBtns[i].checked) {
+        effectLevelValueSlider.noUiSlider.destroy();
+
+        sliderConfig = effectMapList.get(effectNameList[i]);
+        effectValue = sliderConfig.range.min;
+
+        const updateEventHandler = (values) => {
+          effectValue = values[1] - values[0];
+
+          if (effectNameList[i] === 'original') {
+            imgUploadEffectLevelContainer.classList.add('hidden');
+          } else {
+            imgUploadEffectLevelContainer.classList.remove('hidden');
+          }
+
+          switch (effectNameList[i]) {
+            case 'original':
+              imgUploadPreviewContainer.removeAttribute('style');
+              break;
+            case 'blur':
+              imgUploadPreviewContainer.setAttribute('style', `filter: ${effectNameList[i]}(${effectValue}px)`);
+              break;
+            case 'invert':
+              imgUploadPreviewContainer.setAttribute('style', `filter: ${effectNameList[i]}(${effectValue}%)`);
+              break;
+            default:
+              imgUploadPreviewContainer.setAttribute('style', `filter: ${effectNameList[i]}(${effectValue})`);
+              break;
+          }
+
+          effectLevelValueInput.setAttribute('value', effectValue.toFixed(1));
+        };
+
+        noUiSlider.create(effectLevelValueSlider, sliderConfig);
+        effectLevelValueSlider.noUiSlider.on('update', updateEventHandler);
+      }
+    };
+
+    effectsRadioBtns[i].addEventListener('click', radioClickHandler);
+  }
+};
+
+export default effectLevelChanger;
