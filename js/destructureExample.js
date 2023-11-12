d402aff4135a95ede12801f2a067bd7319d83dc4
https://github.com/ghost160111 <jalol100.ghost@yandex.com>
2023-11-12 17:51:28 +0500

    Добавил пример для copy paste готовой деструктуризации всех dom переменных

---
+++ js/destructureExample.js
@@ -0,0 +1,17 @@
+import domVariables from './domVariables.js';
+
+const {
+  body,
+  uploadImgInput,
+  imgUploadOverlay,
+  imgUploadPreview,
+  imgUploadPreviewContainer,
+  cancelImgPreviewBtn,
+  scaleControlSmallerBtn,
+  scaleControlBiggerBtn,
+  scaleControlValue,
+  effectsRadioBtns,
+  effectLevelValueInput ,
+  effectLevelValueSlider,
+  imgUploadEffectLevelContainer,
+} = domVariables;
