803997ed37fc55971956fffb796b1c52eaf46655
Developer <130534010+ghost160111@users.noreply.github.com>
2023-11-13 15:49:21 +0500

    still doing validation with pristine, added 1-4 validations, left 6 checks, 6 different patterns

--- js/validation.js
+++ js/validation.js
@@ -10,14 +10,30 @@ const formHashTags = document.querySelector('.text__hashtags');
 
 const pristine = new Pristine(form, defaultConfig);
 
+const regexPatternChecker = (regex, value) => regex.test(value);
+
 // START VALIDATION OF FORM USING PRISTINE:
 pristine.addValidator(formHashTags, (value) => {
-  const test = (/^[a-zA-Z0-9]{1,20}$/).test(value);
-  if (!test) {
+  const isValid = regexPatternChecker(/^[#a-zA-Z0-9]+$/, value);
+  if (isValid) {
     return true;
   }
 }, 'строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.');
 
+pristine.addValidator(formHashTags, (value) => {
+  const isValid = regexPatternChecker(/^[#a-zA-Z0-9]{1,20}$/, value);
+  if (isValid) {
+    return true;
+  }
+}, 'один хештег может содержать до 20 символов');
+
+pristine.addValidator(formHashTags, (value) => {
+  const isValid = regexPatternChecker(/^[#a-zA-Z0-9]{1,20}$/, value);
+  if (isValid) {
+    const isNotCaseSensitive = (/^a-z$|^A-Z$/).test(value);
+    return isNotCaseSensitive;
+  }
+}, 'хэш-теги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом');
 
 // submit event handler
 form.addEventListener('submit', (event) => {
