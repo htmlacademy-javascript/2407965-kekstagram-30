6f1274327f332cd6c33dd725816563fea2c8f1bd
Developer <130534010+ghost160111@users.noreply.github.com>
2023-11-14 17:41:27 +0500

    finished with validation, now it is turn on async approach

--- js/validation.js
+++ js/validation.js
@@ -1,39 +1,49 @@
+import domVariables from './domVariables.js';
+
+const {
+  form,
+  formHashTags,
+  formDescription
+} = domVariables;
+
 const defaultConfig = {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextClass: 'img-upload__field-wrapper--error'
 };
 
-const form = document.querySelector('.img-upload__form');
-const formHashTags = document.querySelector('.text__hashtags');
-//const formDescription = document.querySelector('.text__description');
-
 const pristine = new Pristine(form, defaultConfig);
 
 const regexPatternChecker = (regex, value) => regex.test(value);
 
+const addFocusedState = (element) => element.classList.add('focused');
+const removeFocusedState = (element) => {
+  if (element.classList.contains('focused')) {
+    element.classList.remove('focused');
+  }
+};
+
+formHashTags.addEventListener('focus' || 'focusin', () => addFocusedState(formHashTags));
+formHashTags.addEventListener('focusout', () => removeFocusedState(formHashTags));
+
+formDescription.addEventListener('focus' || 'focusin', () => addFocusedState(formDescription));
+formDescription.addEventListener('focusout', () => removeFocusedState(formDescription));
+
 // START VALIDATION OF FORM USING PRISTINE:
+
 pristine.addValidator(formHashTags, (value) => {
-  const isValid = regexPatternChecker(/^[#a-zA-Z0-9]+$/, value);
+  const test0 = regexPatternChecker(/^$/i, value);
+  const test1 = regexPatternChecker(/^#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}$/i, value);
+  const test2 = regexPatternChecker(/^#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}\s#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}$/i, value);
+  const test3 = regexPatternChecker(/^#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}\s#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}\s#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}$/i, value);
+  const test4 = regexPatternChecker(/^#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}\s#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}\s#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}\s#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}$/i, value);
+  const test5 = regexPatternChecker(/^#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}\s#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}\s#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}\s#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}\s#[a-zа-яёA-ZА-ЯЁ0-9]{1,19}$/i, value);
+
+  const isValid = test0 || test1 || test2 || test3 || test4 || test5;
   if (isValid) {
     return true;
   }
-}, 'строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.');
-
-pristine.addValidator(formHashTags, (value) => {
-  const isValid = regexPatternChecker(/^[#a-zA-Z0-9]{1,20}$/, value);
-  if (isValid) {
-    return true;
-  }
-}, 'один хештег может содержать до 20 символов');
-
-pristine.addValidator(formHashTags, (value) => {
-  const isValid = regexPatternChecker(/^[#a-zA-Z0-9]{1,20}$/, value);
-  if (isValid) {
-    const isNotCaseSensitive = (/^a-z$|^A-Z$/).test(value);
-    return isNotCaseSensitive;
-  }
-}, 'хэш-теги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом');
+}, 'Ваш хеш-тег может содержать только буквы и цифры, пробелы и спецсимволы запрещены использоваться! Хештеги разделяются пробелами и максимальное их кол-во 5');
 
 // submit event handler
 form.addEventListener('submit', (event) => {
@@ -42,6 +52,7 @@ form.addEventListener('submit', (event) => {
     //console.log('Можно отправлять');
   } else {
     event.preventDefault();
+    // write code that will popup the error messages if there was the error with submitting form
     //console.log('Форма не валидна!');
   }
 });
