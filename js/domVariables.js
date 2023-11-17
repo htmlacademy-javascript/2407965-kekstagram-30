d3955f80828920aef9667a0396d39598507a8e0d
https://github.com/ghost160111 <jalol100.ghost@yandex.com>
2023-11-17 09:43:05 +0500

    added some other variables

--- js/domVariables.js
+++ js/domVariables.js
@@ -18,7 +18,24 @@ const domVariables = {
   imgFiltersBtn: document.querySelectorAll('.img-filters__button'),
   imgFilters: document.querySelector('.img-filters'),
   successTemplate: document.querySelector('.success-template'),
-  failureTemplate: document.querySelector('.error-template')
+  failureTemplate: document.querySelector('.error-template'),
+  pictures: document.querySelectorAll('.picture'),
+  bigPictureOverlay: document.querySelector('.big-picture'),
+  bigPictureCancelBtn: document.querySelector('.big-picture__cancel'),
+  bigPictureImg: document.querySelector('.big-picture__img > img'),
+  socialPicture: document.querySelector('.social__header > .social__picture'),
+  socialCaption: document.querySelector('.social__caption'),
+  likesCount: document.querySelector('.likes-count'),
+  socialCommentShownCount: document.querySelector('.social__comment-shown-count'),
+  socialCommentTotalCount: document.querySelector('.social__comment-total-count'),
+  socialCommentsList: document.querySelector('.social__comments'),
+  socialComment: document.querySelectorAll('.social__comment'),
+  socialCommentAvatar: document.querySelectorAll('.social__comment > .social__picture'),
+  socialCommentText: document.querySelectorAll('.social__comment > .social__text'),
+  socialCommentsLoader: document.querySelector('.social__comments-loader') ?? document.querySelector('.comments-loader'),
+  socialCurrentUserAvatar: document.querySelector('.social__footer > .social__picture'),
+  socialCurrentUserComment: document.querySelector('.social__footer-text'),
+  socialCurrentUserPostComment: document.querySelector('.social__footer-btn')
 };
 
 export default domVariables;
